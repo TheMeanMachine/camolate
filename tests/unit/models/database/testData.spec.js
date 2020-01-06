8f816ba44755cf5736393c84a76e2858db3b393c
TheMeanMachine <machinaaron1@gmail.com>
2020-01-06 12:12:52 +0000

    test testData.spec: add two more tests
    (associateToAuthor)
    All published elements
    Some published elements

--- tests/unit/models/database/testData.spec.js
+++ tests/unit/models/database/testData.spec.js
@@ -58,4 +58,68 @@ describe('associateAuthorsTo', () => {
 		done();
 	})
 
+	test('Valid inputs, all published elements', async done => {
+		
+		const category = [
+			{
+	            name: "Dried Fruit",
+	            published: 1,
+	            authorID: 2,
+	            levelID: 4//bad
+	        },
+	        {
+	            name: "Nuts",
+	            published: 1,
+	            authorID: 2,
+	            levelID: 1//fantastic
+	        },
+	        {
+	            name: "Chocolate",
+	            published: 1,
+	            authorID: 3,
+	            levelID: 5//terrible
+	        },
+		]
+		const listToAddTo = []
+		await testModel.associateAuthorsTo(category, listToAddTo, "categoryID");
+
+		expect(listToAddTo).toEqual([]);
+
+		done();
+	})
+
+
+	test('Valid inputs, some published elements', async done => {
+		
+		const category = [
+			{
+	            name: "Dried Fruit",
+	            published: 1,
+	            authorID: 2,
+	            levelID: 4//bad
+	        },
+	        {
+	            name: "Nuts",
+	            published: 1,
+	            authorID: 2,
+	            levelID: 1//fantastic
+	        },
+	        {
+	            name: "Chocolate",
+	            published: 0,
+	            authorID: 3,
+	            levelID: 5//terrible
+	        },
+		];
+		const listToAddTo = [];
+		await testModel.associateAuthorsTo(category, listToAddTo, "categoryID");
+
+		expect(listToAddTo).toEqual([
+			{
+				categoryID: 3,
+				userID: 3
+			}]);
+
+		done();
+	})
 })
